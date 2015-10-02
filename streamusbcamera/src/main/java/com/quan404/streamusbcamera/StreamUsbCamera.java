ada10e76ed652c958d4b41e041a63e909e8559af
Quan Hua <quanhua92@gmail.com>
2015-10-02 16:02:11 +0700

    Add code to get frame from camera

--- streamusbcamera/src/main/java/com/quan404/streamusbcamera/StreamUsbCamera.java
+++ streamusbcamera/src/main/java/com/quan404/streamusbcamera/StreamUsbCamera.java
@@ -1,37 +1,191 @@
 package com.quan404.streamusbcamera;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class StreamUsbCamera extends AppCompatActivity {
+import android.app.Activity;
+import android.graphics.SurfaceTexture;
+import android.hardware.usb.UsbDevice;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Surface;
+import android.view.View;
+import android.widget.Toast;
+
+import com.serenegiant.usb.CameraDialog;
+import com.serenegiant.usb.USBMonitor;
+import com.serenegiant.usb.UVCCamera;
+import com.serenegiant.widget.UVCCameraTextureView;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class StreamUsbCamera extends Activity implements CameraDialog.CameraDialogParent {
+
+    // for debugging
+    private static String TAG = "SingleCameraPreview";
+    private static boolean DEBUG = true;
+
+    // for thread pool
+    private static final int CORE_POOL_SIZE = 1;		// initial/minimum threads
+    private static final int MAX_POOL_SIZE = 4;			// maximum threads
+    private static final int KEEP_ALIVE_TIME = 10;		// time periods while keep the idle thread
+    protected static final ThreadPoolExecutor EXECUTER
+            = new ThreadPoolExecutor(CORE_POOL_SIZE, MAX_POOL_SIZE, KEEP_ALIVE_TIME,
+            TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+
+    // for accessing USB and USB camera
+    private USBMonitor mUSBMonitor;
+    private UVCCamera mCamera = null;
+    private UVCCameraTextureView mUVCCameraView;
+    private Surface mPreviewSurface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stream_usb_camera);
+
+
+        mUVCCameraView = (UVCCameraTextureView) findViewById(R.id.UVCCameraTextureView);
+        mUVCCameraView.setAspectRatio(UVCCamera.DEFAULT_PREVIEW_WIDTH * 1.0f / UVCCamera.DEFAULT_PREVIEW_HEIGHT);
+        mUVCCameraView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCamera == null) {
+                    CameraDialog.showDialog(StreamUsbCamera.this);
+                } else {
+                    releaseUVCCamera();
+                }
+            }
+        });
+        mUSBMonitor = new USBMonitor(this, mOnDeviceConnectListener);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_stream_usb_camera, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+
+        mUSBMonitor.register();
+        if (mCamera != null)
+            mCamera.startPreview();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    protected void onPause() {
+        mUSBMonitor.unregister();
+        if (mCamera != null)
+            mCamera.stopPreview();
+        super.onPause();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    @Override
+    protected void onDestroy() {
+        if(mUSBMonitor != null){
+            mUSBMonitor.destroy();
+        }
+        if (mCamera != null)
+            mCamera.destroy();
+
+
+        super.onDestroy();
+    }
+
+    private USBMonitor.OnDeviceConnectListener mOnDeviceConnectListener = new USBMonitor.OnDeviceConnectListener() {
+        @Override
+        public void onAttach(UsbDevice device) {
+            if (DEBUG) Log.v(TAG, "onAttach:" + device);
+            Toast.makeText(StreamUsbCamera.this, "USB_DEVICE_ATTACHED", Toast.LENGTH_SHORT).show();
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public void onDettach(UsbDevice device) {
+            if (DEBUG) Log.v(TAG, "onDetach:" + device);
+            Toast.makeText(StreamUsbCamera.this, "USB_DEVICE_DETACHED", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onConnect(UsbDevice device, final USBMonitor.UsbControlBlock ctrlBlock, boolean createNew) {
+            if(mCamera != null) return;
+
+            if (DEBUG) Log.v(TAG, "onConnect: " + device);
+
+            final UVCCamera camera = new  UVCCamera();
+
+            EXECUTER.execute(new Runnable() {
+                @Override
+                public void run() {
+                    // Open Camera
+                    camera.open(ctrlBlock);
+
+
+                    // Set Preview Mode
+                    try {
+                        if (DEBUG) Log.v(TAG, "MJPEG MODE");
+                        camera.setPreviewSize(UVCCamera.DEFAULT_PREVIEW_WIDTH, UVCCamera.DEFAULT_PREVIEW_HEIGHT, UVCCamera.FRAME_FORMAT_MJPEG, 0.5f);
+                    } catch (IllegalArgumentException e1) {
+                        e1.printStackTrace();
+
+                        if (DEBUG) Log.v(TAG, "PREVIEW MODE");
+                        try {
+                            camera.setPreviewSize(UVCCamera.DEFAULT_PREVIEW_WIDTH, UVCCamera.DEFAULT_PREVIEW_HEIGHT, UVCCamera.DEFAULT_PREVIEW_MODE, 0.5f);
+                        } catch (IllegalArgumentException e2) {
+                            if (DEBUG) Log.v(TAG, "CAN NOT ENTER PREVIEW MODE");
+//                            camera.destroy();
+                            releaseUVCCamera();
+                            e2.printStackTrace();
+                        }
+                    }
+
+                    // Start Preview
+                    if (mCamera == null) {
+                        mCamera = camera;
+                        if (mPreviewSurface != null) {
+                            if (DEBUG) Log.v(TAG, "mPreviewSurface.release()");
+                            mPreviewSurface.release();
+                            mPreviewSurface = null;
+                        }
+
+                        final SurfaceTexture st = mUVCCameraView.getSurfaceTexture();
+                        if (st != null) {
+                            if (DEBUG) Log.v(TAG, "mPreviewSurface = new Surface(st);");
+                            mPreviewSurface = new Surface(st);
+                        }
+
+                        camera.setPreviewDisplay(mPreviewSurface);
+                        camera.startPreview();
+                    }
+                }
+            });
+
+        }
+
+        @Override
+        public void onDisconnect(UsbDevice device, USBMonitor.UsbControlBlock ctrlBlock) {
+            if(DEBUG) Log.v(TAG, "onDisconnect" + device);
+            if(mCamera != null && device.equals(mCamera.getDevice())){
+                releaseUVCCamera();
+            }
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+    };
+
+    private void releaseUVCCamera(){
+        if(DEBUG) Log.v(TAG, "releaseUVCCamera");
+        mCamera.close();
+
+        if (mPreviewSurface != null){
+            mPreviewSurface.release();
+            mPreviewSurface = null;
+        }
+        mCamera.destroy();
+        mCamera = null;
+    }
+
+    @Override
+    public USBMonitor getUSBMonitor() {
+        return mUSBMonitor;
     }
 }
